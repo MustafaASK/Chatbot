36d5b3946ebde4c3f91bf3aaa54920533e3d56a2
Phani kumar <phanik@ovahq.com>
2023-07-25 18:56:57 +0530

    upload url issue fix

--- src/shared/api/apiService.ts
+++ src/shared/api/apiService.ts
@@ -10,7 +10,7 @@ import axios from 'axios';
 // import UserData from '../../shared/data/userData';
 
 const Url171 = (process.env.NODE_ENV === "development" || window.location.protocol === 'http:') ? "http://52.40.250.118:8888/webhooks/rest/" : "https://resume.accuick.com/";
-const fileUploadUrl = (process.env.NODE_ENV === "development" || window.location.protocol === 'http:') ? "http://52.40.250.118:8888/http://52.40.250.118:8888/" : "https://resume.accuick.com/";
+const fileUploadUrl = (process.env.NODE_ENV === "development" || window.location.protocol === 'http:') ? "http://52.40.250.118:8888/" : "https://resume.accuick.com/";
 
 const jobSearchUrl = (process.env.NODE_ENV === "development" || window.location.protocol === 'http:') ? "http://52.40.49.193/" : "https://www4.accuick.com/";
 // const Url233 = (process.env.NODE_ENV === "development") ? "http://35.155.228.233:41088/Sequence/api/v1/" : "https://resume.accuick.com/Sequence/api/v1/";
